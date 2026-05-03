3c0d2920ced3cfb3c0bd4aefd8e02a22438abb08
Omar Nouh <onouh7@gmail.com>
2026-05-03 22:28:37 +0300

    modification

---
+++ CinemaDB_GUI/CinemaDB_GUI/DatabaseConfig.cs
@@ -0,0 +1,7 @@
+namespace CinemaDB_GUI
+{
+    public static class DatabaseConfig
+    {
+        public static string ConnectionString { get; } = "Data Source=localhost;Initial Catalog=CinemaDB;Integrated Security=SSPI;TrustServerCertificate=True";
+    }
+}
